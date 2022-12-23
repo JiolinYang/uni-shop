2d922f2517d48b8495d1a34d96aa5fdb50d34a3b
JioLin <JioLinYang@gmail.com>
2022-12-23 21:16:33 +0800

    完成了首页功能

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import Vue from 'vue'
 import App from './App'
+$http.baseUrl = 'https://www.uinav.com'
+//导入网络请求的宝
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+//封装弹框的方法
+uni.$showMsg = function(title ='数据请求失败',duration =1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
